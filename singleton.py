2ddcee0c19ee2e4a81a5cfd076ce53b16d3e425e
Amy Dang <dttmy2020@gmail.com>
2018-01-28 13:43:06 +0700

    Add Singleton pattern

---
+++ singleton.py
@@ -0,0 +1,23 @@
+class Borg:
+
+	_shared_state = {}
+
+	def __init__(self):
+		self.__dict__ = self._shared_state
+
+
+class Singleton(Borg):
+
+	def __init__(self, **kwargs):
+		Borg.__init__(self)
+		self._shared_state.update(kwargs)
+
+
+	def __str__(self):
+		return str(self._shared_state)
+
+x = Singleton(HTTP='Hyper Text Transfer Protocol')
+print(x)
+
+y = Singleton(SNMP='Simple Network Management Protocol')
+print(y)
